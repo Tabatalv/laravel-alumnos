fa7e9c78586ff48e13c41c31c2cba2b9f98f50f7
Tabata <tabata.lopez11@gmail.com>
2025-02-21 10:26:15 +0100

    Añadido menú hamburguesa

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,9 +1,17 @@
 <header class="hidden h-15v bg-header
 md:flex flex-row justify-between items-center
-p-3"><img class="m-4 h-16 max h-full bg-white" src="{{asset('Images/logotipo.png')}}" alt="logo">
+p-3">
+    <img class="m-4 h-16 max-h-full bg-white" src="{{asset('Images/logotipo.png')}}" alt="logo">
     <div>
+        <input type="checkbox" id="menu-toggler" class="peer hidden">
+        <br><br>
+        <label for="menu-toggler" class="text-3xl">
+            &#9778
+        </label>
+
+        <div class="hidden absolute right-0 p-2 peer-checked:flex rounded-xl bg-white">
         @guest
-            <form action="">
+            <form action="" class="flex flex-col">
                 <a href="{{route("login")}}" class="btn btn-secondary btn-sm">Login</a>
                 <a href="{{route("register")}}" class="btn btn-sm mx-2 glass">Register</a>
             </form>
@@ -19,29 +27,38 @@ p-3"><img class="m-4 h-16 max h-full bg-white" src="{{asset('Images/logotipo.png
             </form>
             </div>
         @endauth
-
+    </div>
     </div></header>
 <header class="md:hidden h-15v bg-header
-flex flex-col justify-between items-center
+flex flex-row justify-between
 p-3">
-    <img class="m-4 h-16 max h-full bg-white" src="{{asset('Images/logotipo.png')}}" alt="logo">
-    <h1 class=" text-3xl text-white">GESTION CENTRO</h1>
+    <img class="m-4 h-16 min-h-10 bg-white" src="{{asset('Images/logotipo.png')}}" alt="logo">
+
     <div>
+        <input type="checkbox" id="menu-toggler" class="peer hidden">
+        <br><br>
+        <label for="menu-toggler" class="text-3xl">
+            &#9778
+        </label>
+
+        <div class="hidden absolute right-0 p-4 max-h-full peer-checked:flex rounded-xl bg-white">
         @guest
-            <form action="">
+            <form action="" class="flex flex-col">
                 <a href="{{route("login")}}" class="btn btn-secondary btn-sm">Login</a>
                 <a href="{{route("register")}}" class="btn btn-sm mx-2 glass">Register</a>
             </form>
         @endguest
         @auth
+
             {{auth()->user()->name}}
 
             <form action="{{route("logout")}}" method="post">
                 @csrf
                 <input type="submit" value="Logout" class="btn btn-sm mx-2 glass">
             </form>
+
         @endauth
 
     </div>
-
+    </div>
 </header>
--- resources/views/components/layouts/nav.blade.php
+++ resources/views/components/layouts/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="h-10v bg-nav flex flex-row justify-between items-center px-4">
+<nav class="hidden md:flex h-10v bg-nav flex flex-row justify-between items-center px-4">
     <div>
         <h3 class="text-white font-bold">Gestión Alumnos</h3>
     </div>
@@ -10,5 +10,19 @@
     @auth
         <a href="{{route("alumnos")}}" class="btn btn-sm btn-secondary mx-2">Alumnos</a>
     @endauth
+    </div>
+</nav>
+<nav class="md:hidden h-10v bg-nav flex flex-col justify-around items-center">
+    <div>
+        <h3 class="text-white font-bold">Gestión Alumnos</h3>
+    </div>
+    <div>
+        <button class="btn btn-sm  btn-secondary mx-2">About</button>
+        <button class="btn btn-sm btn-secondary mx-2">Contact</button>
+        <button class="btn btn-sm btn-secondary mx-2">Noticias</button>
+
+        @auth
+            <a href="{{route("alumnos")}}" class="btn btn-sm btn-secondary mx-2">Alumnos</a>
+        @endauth
     </div>
 </nav>
